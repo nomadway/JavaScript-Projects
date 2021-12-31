1e999646340de15dfe6fedb5b67055099284a7e4
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2021-12-31 07:38:24 -0700

    assignment-2

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,7 @@
 
-function superFunction () {
-        var str = "2021 has been a very productive year.";
-        var result = str.fontcolor("red");
-        document.getElementById("Red_Text").innerHTML = result;
+function superFunction () {   //function name
+        var str = "2021 has been a very productive year."; //str is variable
+            str += " And 2022 will be even more fruitful, filled with countless success."; //+= is used to concatenate both sentences.
+        var str = str.fontcolor("red"); //makes font color red
+        document.getElementById("Concatenate").innerHTML = str; //this method calls on id from HTML 
 }
