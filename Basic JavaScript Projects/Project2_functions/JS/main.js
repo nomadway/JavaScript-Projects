0025134f1884bb1d0b5105f5c56eff0c3c9efe88
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2021-12-31 07:26:48 -0700

    assignment-2

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+
+function superFunction () {
+        var str = "2021 has been a very productive year.";
+        var result = str.fontcolor("red");
+        document.getElementById("Red_Text").innerHTML = result;
+}
