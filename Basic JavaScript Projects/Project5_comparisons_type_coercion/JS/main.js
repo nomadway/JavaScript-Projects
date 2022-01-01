10da7bbf305e214951ce07b17655af8416c04872
Uran Bolush <75514107+nomadway@users.noreply.github.com>
2022-01-01 13:22:24 -0700

    assignment-5

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,44 @@
+
+// TASK-1 Utilize the document.write() method and typeof operator to display the data type of a variable of your choosing
+document.write(typeof 100); //number 
+
+document.write("<br>");//space between codes 
+// TASK-2  Write an expression combining a string and a number
+document.write("100" + 50); //string and number 
+
+document.write("<br>"); //space between codes
+
+// TASK-3 Write code using the following operators: ==, ===, >, <, &&, ||, !  and display them in the browser
+//==
+document.write(100==100); //equal 
+document.write("<br>");//space between codes
+
+//===
+document.write(50===50); //comparing 
+document.write("<br>"); //space between codes
+//>
+
+document.write(10>50); //greater than symbol 
+document.write("<br>");//space between codes
+
+//<
+document.write(100<500); //less than symbol
+document.write("<br>"); //space between codes
+
+//&&
+document.write(2<5 && 5<10); //and symbol 
+document.write("<br>"); //space between codes
+
+//||
+document.write(10==10 || 10==20);//or symbol 
+document.write("<br>"); //space between codes
+
+//!
+document.write(20!=10); //20 is not equal to 10
+
+
+
+
+
+
+
